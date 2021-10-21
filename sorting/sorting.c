3039b06890252f661b82b8f6228e3b7d800df8fd
Go <gosha.levinson@gmail.com>
2021-10-21 17:23:55 +0300

    Implemented bubble sort

--- sorting/sorting.c
+++ sorting/sorting.c
@@ -2,8 +2,8 @@
 
 void print_intarr(int[], int);
 void selection(int[], int);
-int* insertion(int[]);
-int* bubble(int[]);
+void insertion(int[]);
+void bubble(int[], int);
 int* merge(int[]);
 
 int main(void)
@@ -12,10 +12,31 @@ int main(void)
     int length = sizeof(list)/sizeof(list[0]);
 
     print_intarr(list, length);
-    selection(list, length);
+    bubble(list, length);
     print_intarr(list, length);
 }
 
+void bubble(int arr[], int length)
+{
+    int swaps = 0;
+    for(int i = 1; i < length; i++)
+    {
+        for(int j = 0; j < length - i; j++)
+        {
+            if(arr[j] > arr[j + 1])
+            {
+                int swap = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = swap;
+                swaps++;
+            }
+        }
+        if(swaps == 0)
+        {
+            break;
+        }
+    }
+}
 void selection(int arr[], int length)
 {
     for(int i = 0; i < length - 1; i++)
