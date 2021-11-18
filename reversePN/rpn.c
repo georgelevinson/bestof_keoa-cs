67251d913961073d6ebbfe3d06ffd40a11a00817
Go <gosha.levinson@gmail.com>
2021-11-18 22:42:27 +0200

    rpn finished

--- reversePN/rpn.c
+++ reversePN/rpn.c
@@ -6,34 +6,51 @@
 int isoper(int);
 int main(int argc, char *argv[])
 {
-    char **stack = malloc(sizeof(char *) * argc);
-    char *etree = malloc(sizeof(char) * (argc - 1));
+    char **stack = malloc(sizeof(char *) * argc/2);
+    char **buffer = malloc(sizeof(char *) * 2);
 
     int sidx = 0;
-    int eidx = 0;
+    int this = 0;
+    int that = 1;
+    char curr;
 
     for(int i = 1; i < argc; i++)
     {
-        char c = *argv[i];
+        curr = *argv[i];
 
-        if(isdigit(c))
+        if(isdigit(curr))
         {
             stack[sidx] = argv[i];
             sidx++;
         }
-        else if(isoper(c))
+        else if(strcmp(argv[i], "+") || strcmp(argv[i], "-") ||
+                strcmp(argv[i], "*") || strcmp(argv[i], "/"))
         {
-            if(eidx)
+            this = this % 2;
+            that = (this ? 0 : 1);
+
+            if(buffer[that] != NULL)
             {
-                // etree[eidx] = malloc(sizeof(char) * 7 + strlen(etree[eidx - 1]));
-                sprintf(etree, "(%s %s %s)", etree, argv[i], stack[sidx - 1]);
+                buffer[this] = malloc(sizeof(char) * (5 + strlen(stack[sidx - 2]) + strlen(stack[sidx - 1])));
             }
             else
             {
-                // etree[eidx] = malloc(sizeof(char) * 7);
-                sprintf(etree, "(%s %s %s)", stack[sidx - 2], argv[i], stack[sidx - 1]);
-                eidx++;
+                buffer[this] = malloc(sizeof(char) * 7);
             }
+
+            if(strcmp(argv[i], "+") || strcmp(argv[i], "-"))
+            {
+                sprintf(buffer[this], "(%s %s %s)", stack[sidx - 2], argv[i], stack[sidx - 1]);
+            }
+            else
+            {
+                sprintf(buffer[this], "%s %s %s", stack[sidx - 2], argv[i], stack[sidx - 1]);
+            }
+            
+            stack[sidx - 2] = buffer[this];
+                
+            stack--;
+            this++;
         }
         else
         {
@@ -42,18 +59,5 @@ int main(int argc, char *argv[])
         }
     }
 
-    printf("%s", etree);
-}
-
-int isoper(int c)
-{
-    if(c == 42 || c == 43 ||
-       c == 45 || c == 47)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
+    printf("%s", buffer[this]);
 }
