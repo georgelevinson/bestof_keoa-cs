d71289ed92f84c40c626c1e8aca1a42dfad123eb
Go <gosha.levinson@gmail.com>
2021-11-19 19:24:40 +0200

    Fixed memory management issues, removed redundant code.

--- reversePN/rpn.c
+++ reversePN/rpn.c
@@ -1,49 +1,85 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include <ctype.h>
+
+// int main(int argc, char *argv[])
+// {
+//     char **stack = (char**) calloc(argc/2, sizeof(char*));
+//     char *buffer = NULL;
+//     int sidx = 0;
+//     char curr;
+
+//     for(int i = 1; i < argc; i++)
+//     {
+//         curr = *argv[i];
+
+//         if(isdigit(curr))
+//         {
+//             stack[sidx] = malloc(sizeof(char));
+//             strcpy(stack[sidx], argv[i]);
+//             sidx++;
+//         }
+//         else if(strcmp(argv[i], "+") || strcmp(argv[i], "-") ||
+//                 strcmp(argv[i], "*") || strcmp(argv[i], "/"))
+//         {
+//             buffer = (char*)calloc(5 + strlen(stack[sidx - 2]) + strlen(stack[sidx - 1]), sizeof(char));
+            
+//             sprintf(buffer, "(%s %s %s)", stack[sidx - 2], argv[i], stack[sidx - 1]);
+//             stack[sidx - 2] = buffer;
+//             free(stack[sidx - 1]);
+//             stack--;
+//         }
+//         else
+//         {
+//             printf("Invalid argument!\n");
+//             return 1;
+//         }
+//     }
+
+//     printf("%s", stack[sidx - 1]);
+// }
+
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 
-int isoper(int);
 int main(int argc, char *argv[])
 {
-    char **stack = malloc(sizeof(char *) * argc/2);
-    char **buffer = malloc(sizeof(char *) * 2);
-
-    int sidx = 0;
-    int this = 0;
-    int that = 1;
-    char curr;
-
-    for(int i = 1; i < argc; i++)
+    char stack[50][200];
+    int sLayer = 1;
+    char *BUFF = (char*)calloc(200, sizeof(char));
+    if(argc < 4)
     {
-        curr = *argv[i];
-
-        if(isdigit(curr))
-        {
-            stack[sidx] = argv[i];
-            sidx++;
-        }
-        else if(strcmp(argv[i], "+") || strcmp(argv[i], "-") ||
-                strcmp(argv[i], "*") || strcmp(argv[i], "/"))
-        {
-            this = this % 2;
-            that = (this ? 0 : 1);
-            
-            buffer[this] = malloc(sizeof(char) * (5 + strlen(stack[sidx - 2]) + strlen(stack[sidx - 1])));
-            
-            sprintf(buffer[this], "(%s %s %s)", stack[sidx - 2], argv[i], stack[sidx - 1]);
-            
-            stack[sidx - 2] = buffer[this];
-                
-            stack--;
-            this++;
-        }
-        else
-        {
-            printf("Invalid argument!");
-            return 1;
-        }
+        printf("Syntax: \n\t./expr [a] [b] [Operator]\n");
     }
+    else
+    {
+        for (int i = 0; i < argc - 1; i++)
+        {
+            char* c = argv[i+1];
 
-    printf("%s", stack[sidx - 1]);
+            if (isdigit(c[0]))
+            {
+                strcpy(stack[sLayer], c);
+                sLayer++;
+            }
+            else
+            {
+                sLayer--;
+                char *p1 = stack[sLayer];
+                sLayer--;
+                char *p2 = stack[sLayer];
+                sprintf(BUFF, "(%s %s %s)", p2, c, p1);
+                strcpy(stack[sLayer], BUFF);
+                sLayer++;
+            }
+        }
+
+        free(BUFF);
+        printf("%s", stack[--sLayer]);
+        printf("\n");
+    }
+   return 0;
 }
